45a7d6ca05299fec4523d755d0cbc74da582eb8f
Michael Hoffman <mike@michaelhoffmaninc.com>
2018-07-02 20:29:50 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# elasticsearch-demo-food-facts
-Elasticsearch Demo Food Facts
+# food-facts
+Demo project for NVISIA Elasticsearch demonstration. Data is sourced from Open Food Facts dataset: https://www.kaggle.com/openfoodfacts/world-food-facts. 
+
+# Project Creation
+
+I used http://start.spring.io to generate the initial project scaffolding. 
+
+I added the following dependencies:
+
+* DevTools
+* Lombok
+* Web
+* Thymeleaf
+* JPA
+* Liquibase
+* Redis
+* Elasticsearch
+* Kafka
+* Kafka Streams
+* Actuator
+
+# Execution
+
+The default command to run is:
+ 
+``` 
+./gradlew bootRun
+```
+
+# Running Elasticsearch
+
+There are many options for running Elasticsearch. In this project, I've included a file src/main/docker/elasticsearch.yml that defines an elasticsearch service. This can be executed using docker-compose to remove the need for custom installation or AWS registration. Elasticsearch can be resource intensive, so its recommended that you increase the resources available to Docker to at least 4-6GB. The current service definition gives ES a heap size of 2GB. 
+
+## Start
+
+```
+docker-compose -f src/main/docker/elasticsearch.yml up -d
+```
+
+If verification fails, you may want to run without -d to see the log and any issue that might have occurred during startup. Most commonly failures in startup are related to memory. 
+
+## Stop
+
+```
+docker-compose -f src/main/docker/elasticsearch.yml down
+```
+
+## Verify
+
+```
+curl http://127.0.0.1:9200/_cat/health
+```
+
+# Running PostgreSQL
+
+Starting:
+
+```
+docker-compose -f src/main/docker/postgresql.yml up -d
+```
+
+Stopping:
+
+```
+docker-compose -f src/main/docker/postgresql.yml down
+```
+
---
+++ src/main/java/com/nvisia/esfoodfacts/ESFoodFactsApp.java
@@ -0,0 +1,50 @@
+package com.nvisia.esfoodfacts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+
+@SpringBootApplication
+public class ESFoodFactsApp {
+
+    private static final Logger log = LoggerFactory.getLogger(ESFoodFactsApp.class);
+
+    private final Environment env;
+
+    public FoodFactsApp(Environment env) {
+        this.env = env;
+    }
+
+    /**
+     * Main method, used to run the application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ESFoodFactsApp.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol,
+            env.getProperty("server.port"),
+            protocol,
+            hostAddress,
+            env.getProperty("server.port"),
+            env.getActiveProfiles());
+    }
+}
